2471ca83107a965afa36746a3d17fcbf9dacdd8e
DarthegX <44780141+DarthegX@users.noreply.github.com>
2019-06-03 18:45:40 +0200

    parser consum

--- app/src/main/java/mycompra/app/logica/parserEstrategia/ParserConsumEstrategia.java
+++ app/src/main/java/mycompra/app/logica/parserEstrategia/ParserConsumEstrategia.java
@@ -1,15 +1,18 @@
 package mycompra.app.logica.parserEstrategia;
 
+import org.xml.sax.Parser;
+
 import java.util.ArrayList;
 
-public class ParserConsumEstrategia implements ParserEstrategia {
+public class ParserConsumEstrategia implements ParserEstrategia
+{
     private static ArrayList<Integer> cantidades;
     private static ArrayList<String> nombres;
     private static ArrayList<Double> precios;
 
     private static int contadorPrecioKilo;
 
-    public ParserConsumEstrategia() {
+    public ParserConsumEstrategia(){
         cantidades = ParserContexto.cantidades;
         nombres = ParserContexto.nombres;
         precios = ParserContexto.precios;
@@ -17,12 +20,18 @@ public class ParserConsumEstrategia implements ParserEstrategia {
         contadorPrecioKilo = ParserContexto.contadorPrecioKilo;
     }
 
-    public void parseProducto(String prod) {
-        if (!Character.isDigit(prod.charAt(2))) {
+    public  void parseProducto(String prod)
+    {
+        if(!Character.isDigit(prod.charAt(2)))
+        {
             cantidades.add(Integer.parseInt(String.valueOf(prod.charAt(0))));
             nombres.add(prod.substring(2));
-        } else if (Character.isDigit(prod.charAt(2))) {
-            if (prod.charAt(6) == 'k') {
+            ParserContexto.numProductosNuevos++;
+        }
+        else if (Character.isDigit(prod.charAt(2)))
+        {
+            if (prod.charAt(6) == 'k')
+            {
                 contadorPrecioKilo++;
                 return;
             }
